d9d92298f7de29e88e6b0765d4e17854b3a556f8
fuhexu <fuhe.xu.52@gmail.com>
2018-05-25 12:15:51 -0400

    removed some more whitespace

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ export default class PickerAndroid extends Component{
 			this._move(dy > this.index * 40 ? this.index * 40 : dy);
 		}else{
 			this._move(dy < (this.index - this.state.items.length + 1) * 40 ? (this.index - this.state.items.length + 1) * 40 : dy);
-		}	
+		}
 	}
 
 	_handlePanResponderRelease(evt, gestureState){
